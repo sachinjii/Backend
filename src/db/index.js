60b9cce81928d4dff82cd55a79530d85f7279cab
Sachin Yadav <yadavsachin9001016801@gmail.com>
2024-02-27 10:26:27 +0530

    MONOGODB Coonected

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const index = async () => {
+  try {
+    // const connectionIstance = await mongoose.connect(
+    //   "mongodb+srv://SachinYadav:sachin1234@cluster0.pq3qgrh.mongodb.net"
+    // );
+
+    const connectionIstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}`
+    );
+    console.log(
+      `\n MongoDB conneced !! DB Host : ${connectionIstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB Connenction FAILED", error);
+    // process.exit(1);
+  }
+};
+
+export default index;
